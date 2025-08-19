90953e16ef4f504675e49f88bde2c5a2fa4f66fc
Fredrik Orderud <fredrik.orderud@gehealthcare.com>
2025-08-19 17:57:01 +0200

    Update to latest version from MS repo.

--- RpcClient/Main.cpp
+++ RpcClient/Main.cpp
@@ -4,52 +4,50 @@
 #include <ctype.h>
 #include "../RpcServer/hello_h.h" 
 #include <windows.h>
-
 #pragma comment(lib, "Rpcrt4.lib")
-
 
 int main()
 {
     RPC_STATUS status;
-    unsigned short* pszUuid = NULL;
+    unsigned short* pszUuid             = NULL;
     unsigned short* pszProtocolSequence = (unsigned short*)L"ncacn_np";
-    unsigned short* pszNetworkAddress = NULL;
-    unsigned short* pszEndpoint = (unsigned short*)L"\\pipe\\hello";
-    unsigned short* pszOptions = NULL;
-    unsigned short* pszStringBinding = NULL;
-    unsigned char* pszString = (unsigned char*)"hello, world";
+    unsigned short* pszNetworkAddress   = NULL;
+    unsigned short* pszEndpoint         = (unsigned short*)L"\\pipe\\hello";
+    unsigned short* pszOptions          = NULL;
+    unsigned short* pszStringBinding    = NULL;
+    unsigned char * pszString           = (unsigned char*)"hello, world";
     unsigned long ulCode;
-
+ 
     status = RpcStringBindingComposeW(pszUuid,
-        pszProtocolSequence,
-        pszNetworkAddress,
-        pszEndpoint,
-        pszOptions,
-        &pszStringBinding);
+                                     pszProtocolSequence,
+                                     pszNetworkAddress,
+                                     pszEndpoint,
+                                     pszOptions,
+                                     &pszStringBinding);
     if (status) exit(status);
 
     status = RpcBindingFromStringBindingW(pszStringBinding, &hello_IfHandle);
-
+ 
     if (status) exit(status);
-
-    RpcTryExcept
+ 
+    RpcTryExcept  
     {
         HelloProc(pszString);
         Shutdown();
     }
-        RpcExcept(1)
+    RpcExcept(1) 
     {
         ulCode = RpcExceptionCode();
         printf("Runtime reported exception 0x%lx = %ld\n", ulCode, ulCode);
     }
     RpcEndExcept
-
-        status = RpcStringFreeW(&pszStringBinding);
-
+ 
+    status = RpcStringFreeW(&pszStringBinding); 
+ 
     if (status) exit(status);
-
+ 
     status = RpcBindingFree(&hello_IfHandle);
-
+ 
     if (status) exit(status);
 
     exit(0);
@@ -58,13 +56,13 @@ int main()
 /******************************************************/
 /*         MIDL allocate and free                     */
 /******************************************************/
-
-void __RPC_FAR* __RPC_USER midl_user_allocate(size_t len)
+ 
+void __RPC_FAR * __RPC_USER midl_user_allocate(size_t len)
 {
     return(malloc(len));
 }
-
-void __RPC_USER midl_user_free(void __RPC_FAR* ptr)
+ 
+void __RPC_USER midl_user_free(void __RPC_FAR * ptr)
 {
     free(ptr);
 }
--- RpcServer/Main.cpp
+++ RpcServer/Main.cpp
@@ -4,72 +4,47 @@
 #include <ctype.h>
 #include "hello_h.h"
 #include <windows.h>
-
 #pragma comment(lib, "Rpcrt4.lib")
 
 int main()
 {
     RPC_STATUS status;
     unsigned short* pszProtocolSequence = (unsigned short*)L"ncacn_np";
-    unsigned short* pszSecurity = NULL;
-    unsigned short* pszEndpoint = (unsigned short*)L"\\pipe\\hello";
+    unsigned short* pszSecurity         = NULL;
+    unsigned short* pszEndpoint         = (unsigned short*)L"\\pipe\\hello";
     unsigned int    cMinCalls = 1;
     unsigned int    fDontWait = FALSE;
-
+ 
     status = RpcServerUseProtseqEpW(pszProtocolSequence,
-        RPC_C_LISTEN_MAX_CALLS_DEFAULT,
-        pszEndpoint,
-        pszSecurity);
-
+                                   RPC_C_LISTEN_MAX_CALLS_DEFAULT,
+                                   pszEndpoint,
+                                   pszSecurity); 
+ 
     if (status) exit(status);
-
-    status = RpcServerRegisterIf(hello_v1_0_s_ifspec,
-        NULL,
-        NULL);
-
+ 
+    status = RpcServerRegisterIf(hello_v1_0_s_ifspec,  
+                                 NULL,   
+                                 NULL); 
+ 
     if (status) exit(status);
-
+ 
     status = RpcServerListen(cMinCalls,
-        RPC_C_LISTEN_MAX_CALLS_DEFAULT,
-        fDontWait);
-
+                             RPC_C_LISTEN_MAX_CALLS_DEFAULT,
+                             fDontWait);
+ 
     if (status) exit(status);
-}
+ }
 
 /******************************************************/
 /*         MIDL allocate and free                     */
 /******************************************************/
-
-void __RPC_FAR* __RPC_USER midl_user_allocate(size_t len)
+ 
+void __RPC_FAR * __RPC_USER midl_user_allocate(size_t len)
 {
     return(malloc(len));
 }
-
-void __RPC_USER midl_user_free(void __RPC_FAR* ptr)
+ 
+void __RPC_USER midl_user_free(void __RPC_FAR * ptr)
 {
     free(ptr);
 }
-
-void  HelloProc(unsigned char* msg) {
-    printf("Message: %s\n", msg);
-}
-
-void Shutdown(void)
-{
-    RPC_STATUS status;
-
-    status = RpcMgmtStopServerListening(NULL);
-
-    if (status)
-    {
-        exit(status);
-    }
-
-    status = RpcServerUnregisterIf(NULL, NULL, FALSE);
-
-    if (status)
-    {
-        exit(status);
-    }
-} //end Shutdown
-
